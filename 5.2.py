acf13fd8ba1a2e86578b008b90d79e2644ada7a6
12ArtuR21 <artur.nuriev134@gmail.com>
2025-06-05 18:10:49 +0400

    Update 5.2.py

--- 5.2.py
+++ 5.2.py
@@ -1,7 +1,29 @@
 from itertools import combinations_with_replacement
 import time
 from collections import Counter
-def generate_bouquets(K, N, max_same):
+def first_part(K, N):
+    print('Усложненная первая часть:')
+    allbuket = []
+    buket = []
+    def backtrack(start, length):
+        if length > N:
+            return
+        if length >= 1:
+            allbuket.append(buket.copy())
+        for flower_type in range(start, K + 1):
+            if buket.count(flower_type) < 1:
+                buket.append(flower_type)
+                backtrack(flower_type, length + 1)
+                buket.pop()
+    backtrack(1, 0)
+    end_time = time.time()
+    most_unique = max(allbuket, key=lambda b: len(set(b)))
+    for i in allbuket:
+        print(i)
+    print('Уникальный букет: ', most_unique)
+    print(f"Время выполнения: {end_time - start_time:.5f} секунд")
+def second_part(K, N, max_same):
+    print('\nУсложненная вторая часть:')
     flower_types = range(1, K + 1)
     valid_bouquets = []
     for m in range(1, N + 1):
@@ -9,16 +31,15 @@ def generate_bouquets(K, N, max_same):
             counts = Counter(bouquet)
             if all(v <= max_same for v in counts.values()):
                 valid_bouquets.append(bouquet)
-    return valid_bouquets
-def most_unique(bouquets):
-    return max(bouquets, key=lambda b: len(set(b)))
+    most_unique = max(valid_bouquets, key=lambda b: len(set(b)))
+    end_time = time.time()
+    for i in valid_bouquets:
+        print(i)
+    print('Уникальный букет: ', most_unique)
+    print(f"Время выполнения: {end_time - start_time:.5f} секунд")
 N = int(input())
 K = int(input())
 start_time = time.time()
-result = generate_bouquets(K, N, 2)
-most_unique = most_unique(result)
-end_time = time.time()
-for i in result:
-    print(i)
-print('Самый уникальный: ', most_unique)
-print(f"Время выполнения: {end_time - start_time:.5f} секунд")
+first_part(K, N)
+start_time = time.time()
+second_part(K, N, 2)
